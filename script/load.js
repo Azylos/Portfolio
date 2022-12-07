dcf26435c60eebd0efdebb8ac1cf8d06b0e45c90
Azylos <44765612+Azylos@users.noreply.github.com>
2022-12-07 17:39:21 +0400

    ajout loader

--- script/load.js
+++ script/load.js
@@ -1,7 +1,12 @@
 const chargement = document.querySelector('.chargement');
+const sec = document.getElementById("chargement");
 
+function attendre(){
+    sec.style.display = 'none'
+}
 window.addEventListener('load', () => {
 
     chargement.classList.add('fondu-out');
-
+    setTimeout(attendre,500);
 })
+
